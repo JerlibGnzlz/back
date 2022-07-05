91156d6a1a9c919188410ba4d841309d340636d7
Tu Nombre <jerlibGnzlz@gmail.com>
2022-07-05 17:44:35 -0300

    mercadopago

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -1,28 +1,36 @@
 const { Order, OrderItem, User } = require("../db");
 
 class PaymentController {
-    constructor(subscriptionService){
-        this.subscriptionService = subscriptionService;
-    }
+  constructor(subscriptionService) {
+    this.subscriptionService = subscriptionService;
+  }
 
-    async getPaymentLink(req,res){
-        try{
-            console.log(req)
-            const payment = await this.subscriptionService.createPayment(req);
-            let user = await User.findOne({where:{email: req.body.user}})
-            let order = await Order.create({state:"pending", total:req.body.total, userId:user.id})
-            await req.body.product.map(async p => {
-                    await OrderItem.create({orderId: order.id, productId: p.id, quantity: p.quantity})
-            })
-            console.log(order)
-            console.log(await OrderItem.findAll())
-            return res.json(payment);
-        } catch (err){
-            console.log(err);
+  async getPaymentLink(req, res) {
+    try {
+      console.log(req);
+      const payment = await this.subscriptionService.createPayment(req);
+      let user = await User.findOne({ where: { email: req.body.user } });
+      let order = await Order.create({
+        state: "pending",
+        total: req.body.total,
+        userId: user.id,
+      });
+      await req.body.product.map(async (p) => {
+        await OrderItem.create({
+          orderId: order.id,
+          productId: p.id,
+          quantity: p.quantity,
+        });
+      });
+      console.log(order);
+      console.log(await OrderItem.findAll());
+      return res.json(payment);
+    } catch (err) {
+      console.log(err);
 
-            return res.status(500).json({error: true, msg: "Failed to create payment"})
-        }
+      return res.status(500).json({ error: true, msg: err });
     }
+  }
 }
 
 module.exports = PaymentController;
