f9189952bf9f51f8bce2d806ef0aa6c8f5925b1e
Tu Nombre <jerlibGnzlz@gmail.com>
2022-07-05 03:25:19 -0300

    PORT

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-               
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
 // const {saveCountries}=require("./src/util/countryCreate")
 // const {saveProducts}=require("./src/util/paymentTypeCreate")
-const {superData}=require("./src/util/superDataCreate")
+const { superData } = require("./src/util/superDataCreate");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-    superData()
+  server.listen(process.env.PORT | 3001, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    superData();
   });
 });
